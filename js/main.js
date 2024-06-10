4857642e830d20d4e44a402ef8c1e73c19b97d71
Adrian <achozas9@hotmail.com>
2024-06-10 20:15:03 +0200

    Cambios con run and debug con suma

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
-numA = 30
-numB = 20
+// Función para sumar dos números
+function sumar(a, b) {
+
+// Error intencional: deberia ser a + b
+return a + b; 
+}
+
+// Prueba de la función sumar
+function testSumar() {
+let num1 = 5;
+ let num2 = 2;
+
+
+// Llamada a la función y almacenamiento del resultado
+ let resultado = sumar(num1, num2)
+
+    
+ // Mostrar el resultado
+console.log("El resultado de sumar " + num1 + " y " + num2 + " es: " + resultado);
+}
+
+testSumar();
+
